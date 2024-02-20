05e9aec50dccc7adf8d93945420db3464b784fe7
zonayovic <108528598+zonayovic@users.noreply.github.com>
2024-02-20 18:51:45 +0300

    feed-back-area

--- project/script.js
+++ project/script.js
@@ -3,6 +3,7 @@ const button = document.getElementById("btn")
 const popupArea = document.getElementById("pop-up-area")
 const main = document.getElementById("main")
 const popBtn = document.getElementById("pop-up-btn")
+const feedBack = document.getElementById("feed-back-text")
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 console.log(inputData , button , popupArea , popBtn)
 const required = document.getElementById("required");
@@ -11,13 +12,16 @@ const required = document.getElementById("required");
 
 let arrData= []
 function getData () {
+    required.classList.add("required")
     if (inputData.value.trim() === "") {
         required.innerText = "E-mail is required";
-        required.classList.add("required")
+        
     } else if (!emailPattern.test(inputData.value)) {
         required.innerText = "E-mail format is not valid";
     } else {
         required.innerText = ""; 
+        feedBack.innerHTML = ` A confirmation email has been sent to <strong> ${inputData.value}</strong>. Please
+        open it and click the button inside to confirm your subscription.`
         arrData.push(inputData.value);
         inputData.value = ""
         console.log(arrData);
